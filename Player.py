5d7d6ac3c528ae830325ba096ff894dac0f6b274
solisoares <solisoares3@gmail.com>
2022-02-17 02:48:39 -0300

    add naive choices to Player.Machine class

--- Player.py
+++ Player.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 from GameBoard import GameBoard
+import random
 
 # TODO 
 # make a move [OK]
+# make players actually update board and let (maybe)
 
 class Player():
     def __init__(self, char: str):
@@ -22,4 +24,18 @@ class Player():
         board.update_board(column_idx, self.char)
 
 class Machine(Player):
-    pass
+    def __init__(self, char: str):
+        """ Construct Machine object by specifying the alphanumeric char."""
+        self.char = char
+
+    def make_move(self, board: GameBoard):
+        """Make a random move on the gamebord.
+        Choose randonly a column and your char will be displayed in the
+        upper most location available, if the column is filled
+        return a message asking a new move.
+
+        Args:
+            board (GameBoard): The game board.
+        """
+        column_idx = random.randint(0, len(board.board) - 1)
+        board.update_board(column_idx, self.char)
