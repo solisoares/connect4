9ba5ab9976cfbe889c5d47462883471b6f3e3f83
solisoares <solisoares3@gmail.com>
2022-02-23 01:46:34 -0300

    update Game structure

--- Game.py
+++ Game.py
@@ -2,8 +2,9 @@ from GameBoard import GameBoard
 from Player import Player, Machine
 
 # Board
-rows = int(input("Please, how many rows do you want the board to be?  "))
-columns = int(input("Please, how many columns do you want the board to be?  "))
+# rows = int(input("Please, how many rows do you want the board to be?  "))
+# columns = int(input("Please, how many columns do you want the board to be?  "))
+rows, columns = 6,7
 board = GameBoard(rows, columns)
 board.show_board()
 
@@ -14,22 +15,16 @@ if mode == 0:
 elif mode == 1:
     p1, p2 = Player("x"), Machine("o")
 
- # Game
+# Game
 while True:
-     column = int(input("Please select a column p1:"))
-     move = False
-     while move == False:
-         move = p1.make_move(column, board)
-         board.show_board()
-    
-     if isinstance(p2, Machine):
-        move = False
-        while move == False:
-            move = p2.make_move(board)
-            board.show_board()
-     else:
-        column = int(input("Please select a column p2:"))
-        move = False
-        while move == False:
-            move = p2.make_move(column, board)
-            board.show_board()
+    column = int(input("Please select a column Player 1:"))
+    p1.make_move(column, board)
+    board.show_board()
+
+    if isinstance(p2, Machine):
+        p2.make_move(board)
+        board.show_board()
+    else:
+        column = int(input("Please select a column Player 2:"))
+        p2.make_move(column, board)
+        board.show_board()
