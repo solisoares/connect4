9134e688db8fc5bc7dded4b5f15eeb4865b3f9a0
solisoares <solisoares3@gmail.com>
2022-04-02 17:46:45 -0300

    update comments in Game.py file
    make comments more descriptive and remove non necessary ones

--- Game.py
+++ Game.py
@@ -1,24 +1,25 @@
+#!/usr/bin/python3
 from GameBoard import GameBoard
 from Player import Player, Machine
 from logo import logo
 
-# Game Logo
+# Print Game Logo
 print(logo)
 
-# Board
-# rows = int(input("Please, how many rows do you want the board to be?  "))
-# columns = int(input("Please, how many columns do you want the board to be?  "))
+# Create Standard Board
 board = GameBoard()
 board.show_board()
 
-# Players
+# Set Game Mode
 mode = int( input( "Do you want to play against a human (0) or a machine (1)?  " ) )
-if mode == 0:
-    p1, p2 = Player("x"), Player("o")
-elif mode == 1:
-    p1, p2 = Player("x"), Machine("o")
 
-# Game
+# Create Players
+if mode == 0:
+    p1, p2 = Player("X"), Player("O")
+elif mode == 1:
+    p1, p2 = Player("X"), Machine("O")
+
+# Game Loop
 while True:
     game_over = board.game_over
     column = int(input("Please select a column Player 1:"))
